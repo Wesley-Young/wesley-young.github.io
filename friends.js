0a1359212a3c0583fe85f3ca1434f812ddd45c4f
校奥浆糊 <wst22@mails.tsinghua.edu.cn>
2025-12-14 15:18:02 +0800

    add Xiaoaojianghu to friends

--- friends.js
+++ friends.js
@@ -5,4 +5,9 @@ export default [
     avatarUrl: "https://blog.erroro.moe/assets/avatar.jpg",
     link: "https://blog.erroro.moe/",
   },
+  {
+      title: "校奥浆糊",
+      link: "https://blog.xiaoaojianghu.fun/",
+      avatar: "https://blog.xiaoaojianghu.fun/img/icon.jpeg"
+  }
 ];
