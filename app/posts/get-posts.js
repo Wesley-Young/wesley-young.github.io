952d34d47e22c9768ef772449fa5871ad0ea2b5c
Wei FeiYang <wesley.f.young@outlook.com>
2025-12-14 11:22:43 +0800

    Exclude special posts in an array

--- app/posts/get-posts.js
+++ app/posts/get-posts.js
@@ -1,12 +1,18 @@
 import { normalizePages } from 'nextra/normalize-pages';
 import { getPageMap } from 'nextra/page-map';
 
+const specialPosts = [
+  'index',
+  'posts',
+  'friends'
+];
+
 export async function getPosts() {
   const { directories } = normalizePages({
     list: await getPageMap(),
     route: '/posts',
   });
   return directories
-    .filter((post) => post.name !== 'index' && post.name !== 'posts')
+    .filter((post) => !specialPosts.includes(post.name))
     .sort((a, b) => new Date(b.frontMatter.date) - new Date(a.frontMatter.date));
 }
